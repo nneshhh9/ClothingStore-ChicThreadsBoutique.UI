2ed35c1d824b0a23e9b2861329588de6888581ef
Vlad Kozhuhov <nneshhh167@gmail.com>
2024-01-03 18:34:54 +0500

    изменение главной страницы, добавление bootstrap, добавление navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path: '',
+  //   component: NotesListComponent
+  // },
+  // {
+  //   path: 'notes',
+  //   component: NotesListComponent
+  // },
+  // {
+  //   path: 'tags',
+  //   component: TagsListComponent
+  // },
+  // {
+  //   path: 'reminders',
+  //   component: RemindersListComponent
+  // }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
